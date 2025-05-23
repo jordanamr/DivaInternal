900d9f3533ad3a0b6d7bd930c26e6737d649d29d
infinite <montilla2808@gmail.com>
2025-05-23 13:18:51 +0200

    Build 23/05

--- cheat/src/appdata/il2cpp-functions.h
+++ cheat/src/appdata/il2cpp-functions.h
@@ -1,56 +1,26 @@
-// Generated C++ file by Il2CppInspector - http://www.djkaty.com - https://github.com/djkaty
-// Target Unity version: 2021.2.0+
-
-// ******************************************************************************
-// * IL2CPP application-specific method definition addresses and signatures
-// ******************************************************************************
-using namespace app;
-
-// C# methods
-DO_APP_FUNC(0x033C6220, String *, Marshal_PtrToStringAnsi, (void* ptr, MethodInfo * method)); 
-
-DO_APP_FUNC(0x033C6240, String *, Marshal_PtrToStringUni, (void* ptr, MethodInfo * method)); 
-
-DO_APP_FUNC(0x03502710, Type *, Type_GetType_3, (String* typeName, MethodInfo * method));
-
-
-DO_APP_FUNC(0x033D8E90, Type *, Assembly_GetType, (Assembly * __this, String* name, bool throwOnError, MethodInfo * method)); 
-
-DO_APP_FUNC(0x033D93B0, Assembly *, Assembly_LoadWithPartialName, (String* partialName, MethodInfo * method)); 
+DO_APP_FUNC(0x033D1830, String*, Marshal_PtrToStringAnsi, (void* ptr, MethodInfo* method));
+DO_APP_FUNC(0x033D1850, String*, Marshal_PtrToStringUni, (void* ptr, MethodInfo* method));
+DO_APP_FUNC(0x0350DD20, Type*, Type_GetType_3, (String* typeName, MethodInfo* method));
+DO_APP_FUNC(0x033E44A0, Type*, Assembly_GetType, (Assembly* __this, String* name, bool throwOnError, MethodInfo* method));
+DO_APP_FUNC(0x033E49C0, Assembly*, Assembly_LoadWithPartialName, (String* partialName, MethodInfo* method));
 
 // Unity methods
-DO_APP_FUNC(0x045067C0, GameObject *, GameObject_Find, (String* name, MethodInfo * method));
-
-
-DO_APP_FUNC(0x0450D000, String *, Object_1_get_name, (Object_1* __this, MethodInfo * method));
-
-
-DO_APP_FUNC(0x0450B750, Object_1 *, Object_1_FindObjectOfType, (Type* type, MethodInfo * method));
-
-
-DO_APP_FUNC(0x0450B3D0, Object_1 *, Object_1_FindAnyObjectByType, (Type* type, MethodInfo * method));
-
-
-DO_APP_FUNC(0x04566690, bool, Input_get_anyKey, (MethodInfo* method));
-
-
+DO_APP_FUNC(0x04511DB0, GameObject*, GameObject_Find, (String* name, MethodInfo* method));
+DO_APP_FUNC(0x045185F0, String*, Object_1_get_name, (Object_1* __this, MethodInfo* method));
+DO_APP_FUNC(0x04516D40, Object_1*, Object_1_FindObjectOfType, (Type* type, MethodInfo* method));
+DO_APP_FUNC(0x045169C0, Object_1*, Object_1_FindAnyObjectByType, (Type* type, MethodInfo* method));
+DO_APP_FUNC(0x04571C80, bool, Input_get_anyKey, (MethodInfo* method));
 
 // Update loop to hook
-DO_APP_FUNC(0x005F2F10, void, AudioManagerListenerPositionProvider_Update, (void* __this, MethodInfo* method));
+DO_APP_FUNC(0x005F26E0, void, AudioManagerListenerPositionProvider_Update, (void* __this, MethodInfo* method));
 
-
-
-// Autopilot
-DO_APP_FUNC(0x0236B4E0, void, dnm_rmo, (dnm* __this, int64_t a, MethodInfo* method));
-
-DO_APP_FUNC(0x0236A9A0, void, dnm_rml, (dnm* __this, int64_t a, bool b, MethodInfo* method));
-
+////// Autopilot
+DO_APP_FUNC(0x02394B50, void, dnm_rmo, (dnm* __this, int64_t a, MethodInfo* method));
+DO_APP_FUNC(0x02393FF0, void, dnm_rml, (dnm* __this, int64_t a, bool b, MethodInfo* method));
 
 // Click cell
-DO_APP_FUNC(0x013B5600, void, InteractiveCellManager_moc, (InteractiveCellManager* __this, bool a, MethodInfo* method));
-
-DO_APP_FUNC(0x013B4430, void, InteractiveCellManager_mnr, (InteractiveCellManager* __this, int32_t a, MethodInfo* method));
-
+DO_APP_FUNC(0x013CF6E0, void, InteractiveCellManager_moc, (InteractiveCellManager* __this, bool a, MethodInfo* method));
+DO_APP_FUNC(0x013CE510, void, InteractiveCellManager_mnr, (InteractiveCellManager* __this, int32_t a, MethodInfo* method));
 
 // Anti AFK
-DO_APP_FUNC(0x011BAA50, void, InputChecker_Update, (InputChecker * __this, MethodInfo * method));
+DO_APP_FUNC(0x011DEA50, void, InputChecker_Update, (InputChecker* __this, MethodInfo* method));
